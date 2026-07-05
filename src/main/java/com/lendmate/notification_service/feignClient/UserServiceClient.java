62957b6c078836207e3a2d69e3dd53b31357fdcd
timurturbil <timurturbil@gmail.com>
2026-07-05 18:13:01 +0300

    rollback last commit

--- src/main/java/com/lendmate/notification_service/feignClient/UserServiceClient.java
+++ src/main/java/com/lendmate/notification_service/feignClient/UserServiceClient.java
@@ -5,11 +5,11 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "user-service")
+@FeignClient(name = "user-service", url = "${user-service.url}")
 public interface UserServiceClient {
     @GetMapping("/user/internal/{id}")
     UserResponse getUserById(@PathVariable Long id);
 
-    @GetMapping("/user/internal/{id}")
+    @GetMapping("/user/internal/{id}/email")
     String getEmailByUserId(@PathVariable Long id);
 }
